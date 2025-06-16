a6400c88997cb1cc3a5050cae838715b0280b554
gigo83 <zzore83@gmail.com>
2025-06-16 15:05:45 +0200

    Dodan logout gumb

--- main.py
+++ main.py
@@ -63,6 +63,12 @@ def login():
             return redirect(url_for('songs'))
     return render_template('login.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
